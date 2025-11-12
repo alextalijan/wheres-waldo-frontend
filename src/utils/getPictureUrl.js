4db2cf4dff79d87a051f999f83e6b2de960ca916
alextalijan <acatalijan@icloud.com>
2025-11-12 14:23:27 -0600

    feat: write logic for getting the url of the picture

---
+++ src/utils/getPictureUrl.js
@@ -0,0 +1,5 @@
+const getPictureUrl = function thatReturnsWaldoPictureUrl(name) {
+  return `https://github.com/alextalijan/wheres-waldo-api/blob/main/assets/pictures/${name}.jpg?raw=true`;
+};
+
+export default getPictureUrl;
