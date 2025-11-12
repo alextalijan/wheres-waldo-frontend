41c239bf811b3eafb63c6b3bf63bcae0ca8c9ab8
alextalijan <acatalijan@icloud.com>
2025-11-12 16:08:06 -0600

    feat: add new route for playing waldo pictures

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import PlayPage from './components/PlayPage/PlayPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/play/:pictureName" element={<PlayPage />} />
       </Routes>
     </BrowserRouter>
   );
