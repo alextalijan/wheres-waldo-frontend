9607db7f1b0d5f875360568371cb9606c21d48a0
alextalijan <acatalijan@icloud.com>
2025-11-13 16:52:18 -0600

    feat: write content for PlayPage, logic for fetching characters to find and records

--- src/components/PlayPage/PlayPage.jsx
+++ src/components/PlayPage/PlayPage.jsx
@@ -1,11 +1,49 @@
 import { useParams } from 'react-router-dom';
 import styles from './PlayPage.module.css';
 import Stopwatch from '../Stopwatch/Stopwatch';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function PlayPage() {
   const { pictureName } = useParams();
-  const [isStopwatchActive, setIsStopwatchActive] = useState(true);
+  const [isGameActive, setIsGameActive] = useState(true);
+  const [characters, setCharacters] = useState([]);
+  const [records, setRecords] = useState([]);
+
+  // Fetch the characters to be found on this page
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API + `pictures/${pictureName}/appearances`)
+      .then((response) => response.json())
+      .then((response) => {
+        if (!response.success) {
+          return alert(response.message);
+        }
+
+        setCharacters(
+          // Filter name and coordinates for each appearance
+          response.appearances.map((appearance) => {
+            return {
+              name: appearance.character.name,
+              xCoordinate: appearance.xCoordinate,
+              yCoordinate: appearance.yCoordinate,
+              isFound: false,
+            };
+          })
+        );
+      });
+  }, [pictureName]);
+
+  // Fetch top 10 record times for the picture
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API + `pictures/${pictureName}/records`)
+      .then((response) => response.json())
+      .then((response) => {
+        if (!response.sucess) {
+          return alert(response.message);
+        }
+
+        setRecords(response.records);
+      });
+  }, [pictureName]);
 
   return (
     <>
@@ -15,7 +53,7 @@ function PlayPage() {
         you found.
       </p>
       <div className={styles['record-times']}>
-        <Stopwatch isActive={isStopwatchActive} />
+        <Stopwatch isActive={isGameActive} />
         <span>Best Time:</span>
       </div>
       <img
@@ -23,6 +61,45 @@ function PlayPage() {
         src={`/src/assets/pictures/${pictureName}.jpg`}
         alt="picture"
       />
+      <section>
+        <h2 className={styles.h2}>Find these characters:</h2>
+        <p className={styles['characters-instructions']}>
+          ❌ means they haven't been found yet.
+        </p>
+        <ul className={styles['characters-list']}>
+          {characters.map((character) => {
+            return (
+              <li>
+                {character.isFound ? '✅ ' : '❌ '}
+                {character.name.charAt(0).toUpperCase() +
+                  character.name.slice(1)}
+                <img
+                  src={`/src/assets/characters/${character.name}.png`}
+                  alt=""
+                  className={styles['character-pic']}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section>
+        <h2 className={styles.h2}>Record times:</h2>
+        {records.length === 0 ? (
+          <p className={styles['no-records-msg']}>No records yet.</p>
+        ) : (
+          <ol>
+            {records.map((record) => {
+              return (
+                <li>
+                  {record.name} - {Math.floor(record.milliseconds / 60000)}m{' '}
+                  {(record.milliseconds % 60000) / 1000}s
+                </li>
+              );
+            })}
+          </ol>
+        )}
+      </section>
     </>
   );
 }
